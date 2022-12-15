4b727ad0bf36073be83c14ae8560a68dbd61d862
lixiaowen <lixiaowen@pharmcube.com>
2022-12-15 16:59:09 +0800

    fix: --

--- src/index.js
+++ src/index.js
@@ -91,13 +91,24 @@ export function useEventListener({
 //     命令行登录 npm login
 //     发布 npm publish
 //     可能会因为名称重复等原因失败，删除包npm unpublish <报名> -force，重新提交
-// 如何使用
-// 在上述的流程中，涉及到一个版本号的问题。需要脱离于git，形成一套记录在项目里的版本记录。 一般来说，推荐使用release来记录。
+// 既然我们的项目发布到了node上面，那它的版本维护也就不能忽视了。但根据我查看一些比较知名的库的npm页面来看。npm方面，只能作为简单的记录版本号。
+// 记录的信息比较少，所以是不能满足我们的需求的。既然npm不行 ，那我们就转换下思路，在其关联的git仓库实现我们需要的功能。
+// 如何使用？
+//在上述的流程中，涉及到一个版本号的问题。需要脱离于git，形成一套记录在项目里的版本记录。 一般来说，推荐使用release来记录。
 // Release是具有changelogs和二进制文件的一级对象，它可以代表超出Git架构本身的一个特定时间点之前的所有项目历史。
 // 也就是通过release，不但能够通过源码体现出项目历史，还能通过已经编译好的二进制文件来进一步描述此时的项目状态。
-// 比如 elementui的维护记录文件。
+// 推荐一种方法，使用releases-it、changelog管理版本号、上传
 
-// 推荐另一种方法，使用releases-it、changelog管理版本号、上传
+//先简单理解release是什么，在了解这个之前，我们需要先知道tag是什么。
+//tag，中文名：标签，    
+// tag是git给我们提供的用于管理记录版本的功能。比如，我每次在提交时，需要将其设定一个版本（2.0.0），git tag v1.1.0
+//，同时将其记录下来，推送到远程， git push origin --tags 或者 git push origin v1.0.0
+//；在我需要翻看之前的记录（1.0.0，。。）的时候，我就都能查看到。
+// git show v1.0.0
+
+ // release 则是脱离了git本身概念的功能，可以说是代码托管平台（github等）提供的更高层的服务。一般在提交时，会要求填写 tag 名、分支以及相应的发布说明，还可上传编译好的程序、打包好的文件等。
+ //在上述TAG的功能上，它额外的提供添加编译好的二进制文件以及其他更多信息等。可以让用户在托管平台，以可视化的方式，获取
+ // 更多的版本信息。比如elementUI的release功能
 
 // npm i -D release-it
 
@@ -148,11 +159,11 @@ export function useEventListener({
 
 // 使用--only-version可以仅使用提示符来确定版本，并自动执行其余操作。
 
-// 正常流程：变更了一部分代码，一般都要先提交 推送到代码服务器，先commit,不要push
+// 正常流程：变更了一部分代码，一般都要先提交再推送到代码服务器。但我们这边，先commit,不要push
 // git add .
 // git commit -m 'feat: release-it'
 // 或者安装git-cz，规范commit信息， 执行 npm install git-cz -D ，并修改npm scripts 增加 "commit": "git-cz"
-// run realease 
+// run release 
 
 // 这里生成了changelog但是没有地方进行记录,于是release-it/conventional-changelog
 // 安装release-it/conventional-changelog
